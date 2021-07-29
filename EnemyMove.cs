8e22c3839e9bb0d8048243e7334a735a9024e9f4
Nikita <61244906+NikllX@users.noreply.github.com>
2021-07-29 19:29:36 +0300

    Движение противника

---
+++ EnemyMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction.normalized * Time.deltaTime * moveSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D ColAsteroid)
+    {
+        if (ColAsteroid.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (ColAsteroid.gameObject.tag == "PlayerBullet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
