e1e3074b9b883ebc656e26edbddfb88e8a0530f1
Nikita <61244906+NikllX@users.noreply.github.com>
2021-07-29 19:38:03 +0300

    Движение вражеского снаряда

---
+++ MoveObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public Vector2 direction;
+    void Update()
+    {
+        transform.Translate(direction.normalized * Time.deltaTime * moveSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D ColAsteroid)
+    {
+        if (ColAsteroid.gameObject.tag == "Bullet")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (ColAsteroid.gameObject.tag == "Zone")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
