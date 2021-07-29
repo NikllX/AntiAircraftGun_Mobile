ea7d22b25a2461ebfd76a20da5521dcf8c7024cd
Nikita <61244906+NikllX@users.noreply.github.com>
2021-07-29 19:32:23 +0300

    Спавн противника

---
+++ EnemySpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject EnemyPlane;
+    public GameObject Spawn;
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+    IEnumerator Spawner()
+    {
+        while (true)
+        {
+            float randSec = Random.Range(1.5f, 4f);
+
+            yield return new WaitForSeconds(randSec);
+            if (GameObject.Find("Player") != null)
+            {
+                GameObject newEnemyPlane = Instantiate(EnemyPlane, new Vector3(Spawn.transform.position.x, Spawn.transform.position.y, 0), Quaternion.identity);
+                Destroy(newEnemyPlane, 10);
+            }
+        }
+    }
+}
