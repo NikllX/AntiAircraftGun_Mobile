3cf670aaa758e4a431d3cf46425e9136a6cfa76b
Nikita <61244906+NikllX@users.noreply.github.com>
2021-07-29 19:11:58 +0300

    Движение пули игрока

---
+++ Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D ColAsteroid)
+    {
+        if (ColAsteroid.gameObject.tag == "Enemy")
+        {
+            Destroy(ColAsteroid.gameObject);
+            Destroy(this.gameObject);
+        }
+        if (ColAsteroid.gameObject.tag == "Zone")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
