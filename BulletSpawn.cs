7819e03af0f429d223ee48328e4946cef40018d7
Nikita <61244906+NikllX@users.noreply.github.com>
2021-07-29 19:16:32 +0300

    Спавн пулей игрока

---
+++ BulletSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    public GameObject Bullet;
+    public GameObject Spaw;
+    public int timeStartR = 2;
+    public int timeEndR = 5;
+    void Start()
+    {
+        StartCoroutine(Spawner());
+
+    }
+    IEnumerator Spawner()
+    {
+        while (true)
+        {
+            int randSec = Random.Range(timeStartR, timeEndR);
+            yield return new WaitForSeconds(randSec);
+            if (GameObject.Find("Player") != null)
+            {
+                var rotation = Quaternion.Euler(0, 0, 0);
+                GameObject newBullet = Instantiate(Bullet, new Vector3(Spaw.transform.position.x , Spaw.transform.position.y, 0), rotation);
+                Destroy(newBullet, 5);
+            }
+        }
+    }
+}
