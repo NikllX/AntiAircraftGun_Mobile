ba71c6bbc42b9aa472cbfdca39b21a10fa432740
Nikita <61244906+NikllX@users.noreply.github.com>
2021-07-29 19:36:13 +0300

    Движение травы

---
+++ MoveGross.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGross : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public Vector2 direction;
+
+    void Update()
+    {
+        transform.Translate(direction.normalized * Time.deltaTime * moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D ColAsteroid)
+    {
+        if (ColAsteroid.gameObject.tag == "Zone")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
